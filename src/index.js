16ced35de436db7cd895be1ac9135299a2678de4
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-12 12:59:30 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -2,128 +2,121 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
-const { connection } = require('./connector')
 
-const { covidModel } = require('./models/covid');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-
-mongoose.connect('mongodb://localhost:27017/covid', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+const { connection } = require('./connector')
+const {data} = require('./data');
 
 app.get('/totalRecovered', async (req, res) => {
-    const result = await covidModel.aggregate([
-      {
-        $group: {
-          _id: 'total',
-          recovered: {
-            $sum: '$recovered',
-          },
+  const result = await covidModel.aggregate([
+    {
+      $group: {
+        _id: 'total',
+        recovered: {
+          $sum: '$recovered',
         },
       },
-    ]);
-    res.send({ data: result[0] });
-  });
-  
-  app.get('/totalActive', async (req, res) => {
-    const result = await covidModel.aggregate([
-      {
-        $group: {
-          _id: 'total',
-          active: {
-            $sum: { $subtract: ['$infected', '$recovered'] },
-          },
+    },
+  ]);
+  res.send({ data: result[0] });
+});
+
+app.get('/totalActive', async (req, res) => {
+  const result = await covidModel.aggregate([
+    {
+      $group: {
+        _id: 'total',
+        active: {
+          $sum: { $subtract: ['$infected', '$recovered'] },
         },
       },
-    ]);
-    res.send({ data: result[0] });
-  });
-  
-  app.get('/totalDeath', async (req, res) => {
-    const result = await covidModel.aggregate([
-      {
-        $group: {
-          _id: 'total',
-          death: {
-            $sum: '$death',
-          },
+    },
+  ]);
+  res.send({ data: result[0] });
+});
+
+app.get('/totalDeath', async (req, res) => {
+  const result = await covidModel.aggregate([
+    {
+      $group: {
+        _id: 'total',
+        death: {
+          $sum: '$death',
         },
       },
-    ]);
-    res.send({ data: result[0] });
-  });
-  
-  app.get('/hotspotStates', async (req, res) => {
-    const result = await covidModel.aggregate([
-      {
-        $project: {
-          state: 1,
-          rate: {
-            $round: [
-              {
-                $divide: [
-                  {
-                    $subtract: ['$infected', '$recovered'],
-                  },
-                  '$infected',
-                ],
-              },
-              5,
-            ],
-          },
+    },
+  ]);
+  res.send({ data: result[0] });
+});
+
+app.get('/hotspotStates', async (req, res) => {
+  const result = await covidModel.aggregate([
+    {
+      $project: {
+        state: 1,
+        rate: {
+          $round: [
+            {
+              $divide: [
+                {
+                  $subtract: ['$infected', '$recovered'],
+                },
+                '$infected',
+              ],
+            },
+            5,
+          ],
         },
       },
-      {
-        $match: {
-          rate: {
-            $gt: 0.1,
-          },
+    },
+    {
+      $match: {
+        rate: {
+          $gt: 0.1,
         },
       },
-      {
-        $sort: {
-          rate: -1,
+    },
+    {
+      $sort: {
+        rate: -1,
+      },
+    },
+  ]);
+  res.send({ data: result });
+});
+
+app.get('/healthyStates', async (req, res) => {
+  const result = await covidModel.aggregate([
+    {
+      $project: {
+        state: 1,
+        mortality: {
+          $round: [
+            {
+              $divide: ['$death', '$infected'],
+            },
+            5,
+          ],
         },
       },
-    ]);
-    res.send({ data: result });
-  });
-  
-  app.get('/healthyStates', async (req, res) => {
-    const result = await covidModel.aggregate([
-      {
-        $project: {
-          state: 1,
-          mortality: {
-            $round: [
-              {
-                $divide: ['$death', '$infected'],
-              },
-              5,
-            ],
-          },
+    },
+    {
+      $match: {
+        mortality: {
+          $lt: 0.005,
         },
       },
-      {
-        $match: {
-          mortality: {
-            $lt: 0.005,
-          },
-        },
+    },
+    {
+      $sort: {
+        mortality: 1,
       },
-      {
-        $sort: {
-          mortality: 1,
-        },
-      },
-    ]);
-    res.send({ data: result });
-  });
+    },
+  ]);
+  res.send({ data: result });
+});
 
 
 
