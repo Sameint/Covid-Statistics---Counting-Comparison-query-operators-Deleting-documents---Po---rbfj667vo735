8f72986a8e627356166d9cac8226cb4c62d02c81
Ram Narayan <shukla.narayanram@gmail.com>
2023-05-12 13:13:41 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,121 +2,96 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
+const roundTo = require('round-to');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connection } = require('./connector')
+
 const {data} = require('./data');
-
-app.get('/totalRecovered', async (req, res) => {
-  const result = await covidModel.aggregate([
-    {
-      $group: {
-        _id: 'total',
-        recovered: {
-          $sum: '$recovered',
-        },
-      },
-    },
-  ]);
-  res.send({ data: result[0] });
+app.get('/totalRecovered', (req, res) => {
+    let recoveredCount = 0;
+    data.map(item => {
+        recoveredCount += item.recovered;
+    });
+    res.send({
+        data: {
+            _id: "total",
+            recovered: recoveredCount
+        }
+    });
+    return;
 });
 
-app.get('/totalActive', async (req, res) => {
-  const result = await covidModel.aggregate([
-    {
-      $group: {
-        _id: 'total',
-        active: {
-          $sum: { $subtract: ['$infected', '$recovered'] },
-        },
-      },
-    },
-  ]);
-  res.send({ data: result[0] });
+app.get('/totalActive', (req, res) => {
+    let activeCount = 0;
+    data.map(item => {
+        activeCount += (item.infected - item.recovered);
+    });
+    res.send({
+        data: {
+            _id: "total",
+            active: activeCount
+        }
+    });
+    return;
 });
 
-app.get('/totalDeath', async (req, res) => {
-  const result = await covidModel.aggregate([
-    {
-      $group: {
-        _id: 'total',
-        death: {
-          $sum: '$death',
-        },
-      },
-    },
-  ]);
-  res.send({ data: result[0] });
+app.get('/totalDeath', (req, res) => {
+    let deathCount = 0;
+    data.map(item => {
+        deathCount += item.death;
+    });
+    res.send({
+        data: {
+            _id: "total",
+            death: deathCount
+        }
+    });
+    return;
 });
 
-app.get('/hotspotStates', async (req, res) => {
-  const result = await covidModel.aggregate([
-    {
-      $project: {
-        state: 1,
-        rate: {
-          $round: [
-            {
-              $divide: [
-                {
-                  $subtract: ['$infected', '$recovered'],
-                },
-                '$infected',
-              ],
-            },
-            5,
-          ],
-        },
-      },
-    },
-    {
-      $match: {
-        rate: {
-          $gt: 0.1,
-        },
-      },
-    },
-    {
-      $sort: {
-        rate: -1,
-      },
-    },
-  ]);
-  res.send({ data: result });
+app.get('/hotspotStates', (req, res) => {
+    let hotspotStates = [];
+    data.map(item => {
+        let infected = item.infected;
+        let recovered = item.recovered;
+
+        if((infected - recovered) / infected > 0.1) {
+            let obj = {
+                state : item.state,
+                rate : roundTo((infected - recovered) / infected, 5)
+            }
+            hotspotStates.push(obj);
+        }
+    });
+    res.send({
+        data: hotspotStates.map(item => item)
+    });
+    return;
 });
 
-app.get('/healthyStates', async (req, res) => {
-  const result = await covidModel.aggregate([
-    {
-      $project: {
-        state: 1,
-        mortality: {
-          $round: [
-            {
-              $divide: ['$death', '$infected'],
-            },
-            5,
-          ],
-        },
-      },
-    },
-    {
-      $match: {
-        mortality: {
-          $lt: 0.005,
-        },
-      },
-    },
-    {
-      $sort: {
-        mortality: 1,
-      },
-    },
-  ]);
-  res.send({ data: result });
+app.get('/healthyStates', (req, res) => {
+    let healthyStates = [];
+    data.map(item => {
+        let infected = item.infected;
+        let death = item.death;
+
+        if(death / infected < 0.005) {
+            let obj = {
+                state : item.state,
+                mortality : roundTo(death / infected, 5)
+            }
+            healthyStates.push(obj);
+        }
+    });
+    res.send({
+        data: healthyStates.map(item => item)
+    });
+    return;
 });
+
 
 
 
